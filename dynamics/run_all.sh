317434d9659eadf9c2eaadc1bfe43832752fd533
Christina Lu <christina.ty.lu@gmail.com>
2025-11-12 22:29:10 +0000

    Bash script for llama escalation case studies.

--- dynamics/run_all.sh
+++ dynamics/run_all.sh
@@ -12,17 +12,21 @@ FILENAMES=(
 	"self_medication"
 	"wellness"
 )
-EXP_ID="layers_46:54-p0.25"
+EXP_ID="layers_32:56-p0.25"
 
 for filename in "${FILENAMES[@]}"; do
 	ts -G 2 uv run 0_auto_conversation.py \
     --instruction-file /root/git/persona-subspace/dynamics/data/escalation/${filename}.txt \
-    --target-model meta-llama/Llama-3.1-8B-Instruct --num-turns 30 --output-dir /root/git/persona-subspace/dynamics/results/llama-3.3-70b/kimi-k2
-
-	# ts -G 1 uv run scripts/steer_transcript.py \
-	# 	--transcript /root/git/persona-subspace/dynamics/results/qwen-3-32b/kimi-k2/${filename}.json \
-	# 	--config /workspace/qwen-3-32b/capped/configs/contrast/role_trait_sliding_config.pt \
-	# 	--experiment_id $EXP_ID \
-	# 	--output_file /root/git/persona-subspace/dynamics/results/qwen-3-32b/steered/${EXP_ID}/${filename}.json \
-	# 	--model_name Qwen/Qwen3-32B
+    --target-model meta-llama/Llama-3.3-70B-Instruct --num-turns 30 \
+	--output-dir /root/git/persona-subspace/dynamics/results/llama-3.3-70b/gpt-5 \
+	--auditor-model openai/gpt-5-chat
 done
+
+# for filename in "${FILENAMES[@]}"; do
+# 	ts -G 2 uv run scripts/steer_transcript.py \
+# 		--transcript /root/git/persona-subspace/dynamics/results/llama-3.3-70b/kimi-k2/${filename}.json \
+# 		--config /workspace/llama-3.3-70b/capped/configs/contrast/role_trait_config.pt \
+# 		--experiment_id $EXP_ID \
+# 		--output_file /root/git/persona-subspace/dynamics/results/llama-3.3-70b/steered/${EXP_ID}/${filename}.json \
+# 		--model_name meta-llama/Llama-3.3-70B-Instruct
+# done
